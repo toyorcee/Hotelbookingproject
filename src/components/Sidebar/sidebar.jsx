82bbf8b45f63ba15aa8d5298c0b5f82fa53dbdcd
unknown <oluwatoyosiolaniyan@gmail.com>
2024-07-13 12:29:31 +0200

    final push

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -5,7 +5,7 @@ import img1 from "../../assets/img4.png";
 import img2 from "../../assets/img5.png";
 import img3 from "../../assets/img6.png";
 import img4 from "../../assets/img7.jpeg";
-import map from "../../assets/yellow-world-map.webp"
+import map from "../../assets/yellow-world-map.webp";
 import {
   IoHomeOutline,
   IoBarChartOutline,
@@ -20,26 +20,32 @@ export default function Side() {
     {
       name: "DASHBOARD",
       icon: <IoHomeOutline style={{ color: "#A68B3D" }} size={25} />,
+      isActive: false,
     },
     {
       name: "FLIGHTS",
       icon: <PiAirplaneTiltLight style={{ color: "#A68B3D" }} size={25} />,
+      isActive: true, 
     },
     {
       name: "WALLET",
       icon: <TfiWallet style={{ color: "#A68B3D" }} size={25} />,
+      isActive: false,
     },
     {
       name: "REPORTS",
       icon: <IoBarChartOutline style={{ color: "#A68B3D" }} size={25} />,
+      isActive: false,
     },
     {
       name: "STATISTICS",
       icon: <AiOutlinePieChart style={{ color: "#A68B3D" }} size={25} />,
+      isActive: false,
     },
     {
       name: "SETTINGS",
       icon: <IoSettingsOutline style={{ color: "#A68B3D" }} size={25} />,
+      isActive: false,
     },
   ];
 
@@ -51,11 +57,22 @@ export default function Side() {
           <h1 className="name">ALEX JOHNSON</h1>
           <span className="email">alex.johnson@gmail.com</span>
         </div>
-        <div className="flex flex-col w-11/12 mt-4 items justify-between">
+        <div className="flex flex-col w-full items justify-between">
           {menuItems.map((item, index) => (
-            <div className="flex items-center gap-7 my-2 mx-10 cursor-pointer" key={index}>
+            <div
+              className={`flex items-center gap-7 my-1 mx-10 cursor-pointer ${
+                item.isActive ? "bg-[#E1ECEB] text-black rounded-full" : ""
+              }`}
+              key={index}
+            >
               {item.icon}
-              <p className="itemp cursor-pointer">{item.name}</p>
+              <p
+                className={`itemp cursor-pointer ${
+                  item.isActive ? "font-bold" : ""
+                }`}
+              >
+                {item.name}
+              </p>
             </div>
           ))}
         </div>
@@ -82,7 +99,7 @@ export default function Side() {
               +70
             </span>
           </div>
-          <img src={map} alt="map" style={{ width: "100%", height: "26%" }}/>
+          <img src={map} alt="map" style={{ width: "100%", height: "26%" }} />
         </div>
       </div>
     </section>
